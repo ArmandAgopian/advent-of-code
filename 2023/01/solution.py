d90d5a8e4fe575eb358f48d0dab798043ad787d9
Armand Agopian <armandagopian457@gmail.com>
2024-04-09 04:59:19 -0700

    2023 Day 01 part 2

--- 2023/01/solution.py
+++ 2023/01/solution.py
@@ -1,16 +1,26 @@
-import re
+import regex as re
 
 LETTER_DICT = {
-    "zero": "0",
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9",
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
 }
 
 
@@ -33,9 +43,12 @@ def part01(lines):
 
 
 def part02(lines):
-    lines[:] = [re.sub(r"zero|one|two|three|four|five|six|seven|eight|nine", lambda x: LETTER_DICT.get(x.group()), line) for line in lines]
-    print(lines)
-    return part01(lines)
+    value = 0
+    for line in lines:
+        matches = re.findall(r"\d|zero|one|two|three|four|five|six|seven|eight|nine", line, overlapped=True)
+        value += ((LETTER_DICT[matches[0]] * 10) + LETTER_DICT[matches[-1]])
+        
+    return value
 
 
 if __name__ == "__main__":
