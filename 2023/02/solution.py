46f8b9d51d6925a8083db4132ac527ab47e02971
Armand Agopian <armandagopian457@gmail.com>
2024-04-10 13:07:35 -0700

    Day 2 init

---
+++ 2023/02/solution.py
@@ -0,0 +1,17 @@
+import regex as re
+
+RED_MAX = 12
+GREEN_MAX = 13
+BLUE_MAX = 14
+
+def main():
+    with open("input.txt") as f:
+        lines = f.read().splitlines()
+
+    print(f"Part 01: {part01(lines)}")
+    print(f"Part 02: {part02(lines)}")
+
+def part01(lines):
+
+def part02(lines):
+    
