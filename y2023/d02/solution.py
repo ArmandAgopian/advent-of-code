af6cc39554f0edbab81b312cdbc1e84458246211
Armand Agopian <armandagopian457@gmail.com>
2024-04-15 20:29:19 -0700

    Day 02

--- y2023/d02/solution.py
+++ y2023/d02/solution.py
@@ -1,8 +1,10 @@
 import regex as re
 
-RED_MAX = 12
-GREEN_MAX = 13
-BLUE_MAX = 14
+MAX_VAL = {
+    'red': 12,
+    'green': 13,
+    'blue': 14
+}
 
 def main():
     with open("./input.txt") as f:
@@ -12,9 +14,44 @@ def main():
     print(f"Part 02: {part02(lines)}")
 
 def part01(lines):
-    return None
+    value = 0
+    for count, line in enumerate(lines, start=1):
+        limit = False
+
+        # Each game set
+        for set in line.split(':')[1].split(';'):
+
+            # Each color number pair
+            for pair in set.split(','):
+                number, color = pair.split()
+                if int(number) > MAX_VAL.get(color):
+                    limit = True  
+
+        if not limit:
+            value += count
+
+    return value
+
 def part02(lines):
-    return None
+    value = 0
+    for count, line in enumerate(lines, start=1):
+        maxes = {
+            "red": 0,
+            "blue": 0,
+            "green":0
+        }
+
+        # Each game set
+        for set in line.split(':')[1].split(';'):
+
+            # Each color number pair
+            for pair in set.split(','):
+                number, color = pair.split()
+                maxes.update({color: max(maxes.get(color), int(number))})
+
+        value += (maxes.get('red') * maxes.get('blue') * maxes.get('green'))
+        
+    return value
 
 if __name__ == '__main__':
     main()
