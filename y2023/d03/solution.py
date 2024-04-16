b3c62215ed38282b65231d88aa9884b9bdb7ecd3
Armand Agopian <armandagopian457@gmail.com>
2024-04-15 20:51:33 -0700

    Setup Day 03

---
+++ y2023/d03/solution.py
@@ -0,0 +1,21 @@
+import regex as re
+
+def main():
+    with open("./input.txt") as f:
+        lines = f.read().splitlines()
+
+    print(f"Part 01: {part01(lines)}")
+    print(f"Part 02: {part02(lines)}")
+
+def part01(lines):
+    value = 0
+
+    return value
+
+def part02(lines):
+    value = 0
+        
+    return value
+
+if __name__ == '__main__':
+    main()
---
+++ y2023/d03/test_solution.py
@@ -0,0 +1,12 @@
+from y2023.d03 import solution
+
+
+def test_part01():
+    example = []
+
+    assert solution.part01(example) == 8
+
+def test_part02():
+    example = []
+
+    assert solution.part02(example) == 2286
