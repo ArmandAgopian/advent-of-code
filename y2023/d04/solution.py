d32c349fb5435d71d00986a36fe036b04fc6f471
Armand Agopian <armandagopian457@gmail.com>
2024-04-28 00:41:13 -0700

    d04 p01 complete

--- y2023/d04/solution.py
+++ y2023/d04/solution.py
@@ -4,6 +4,13 @@ import regex as re
 def part01(lines):
     value = 0
 
+    for line in lines:
+        winning_nums = set(line.split(":")[1].split("|")[0].split())
+        nums = set(line.split(":")[1].split("|")[1].split())
+        actual_wins = winning_nums & nums
+        if actual_wins:
+            value += 2 ** (len(actual_wins) - 1)
+
     return value
 
 
