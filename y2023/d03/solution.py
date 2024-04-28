95e113f9531e124f14f2208f2a8b6df919703d75
Armand Agopian <armandagopian457@gmail.com>
2024-04-27 23:46:39 -0700

    Finish Day 3 pt 2

--- y2023/d03/solution.py
+++ y2023/d03/solution.py
@@ -54,7 +54,22 @@ def part01(lines):
 
 def part02(lines):
     value = 0
+    valid_parts = {}
 
+    for i, line in enumerate(lines):
+        for gear in re.finditer(r"\*", line):
+            valid_parts[(i, gear.start())] = []
+
+    for i, line in enumerate(lines):
+        for num in re.finditer(r"\d+", line):
+            for row in range(i - 1, i + 2):
+                for col in range(num.start() - 1, num.end() + 1):
+                    if (row, col) in valid_parts:
+                        valid_parts[(row, col)].append(int(num.group()))
+
+    for part in valid_parts.values():
+        if len(part) == 2:
+            value += math.prod(part)
     return value
 
 
