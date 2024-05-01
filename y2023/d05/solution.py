cd4edc0612becb073c68eab4ffc4272412b14e21
Armand Agopian <armandagopian457@gmail.com>
2024-05-01 03:56:36 -0700

    day 5 pt 1 complete

--- y2023/d05/solution.py
+++ y2023/d05/solution.py
@@ -2,8 +2,19 @@ import regex as re
 
 
 def part01(lines):
-    value = 0
+    value = float("inf")
+    groups = lines.split("\n\n")
+    for seed in groups[0].split(":")[1].split():
+        val = int(seed)
 
+        for group in map(lambda x: x.split(":")[1], groups[1:]):
+            for line in map(lambda x: x.split(), group.strip().splitlines()):
+                new, old, offset = map(lambda x: int(x), line)
+                if old <= val < old + offset:
+                    val = new + (val - old)
+                    break
+
+        value = min(val, value)
     return value
 
 
@@ -15,7 +26,7 @@ def part02(lines):
 
 if __name__ == "__main__":
     with open("./input.txt") as f:
-        lines = f.read().splitlines()
+        lines = f.read()
 
     print(f"Part 01: {part01(lines)}")
     print(f"Part 02: {part02(lines)}")
