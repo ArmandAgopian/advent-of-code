c6fa48d5f5645c93bf02558f23b2704bb398b488
Armand Agopian <armandagopian457@gmail.com>
2024-05-03 02:50:18 -0700

    prep day 6

---
+++ y2023/d06/solution.py
@@ -0,0 +1,21 @@
+import regex as re
+
+
+def part01(lines):
+    value = 0
+
+    return value
+
+
+def part02(lines):
+    value = 0
+
+    return value
+
+
+if __name__ == "__main__":
+    with open("./input.txt") as f:
+        lines = f.readlines()
+
+    print(f"Part 01: {part01(lines)}")
+    print(f"Part 02: {part02(lines)}")
