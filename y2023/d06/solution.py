f8cdeef9d10c51d30427216b7763aa3823c83380
Armand Agopian <armandagopian457@gmail.com>
2024-05-03 03:25:58 -0700

    finish day 6

--- y2023/d06/solution.py
+++ y2023/d06/solution.py
@@ -2,13 +2,31 @@ import regex as re
 
 
 def part01(lines):
-    value = 0
+    value = 1
+    times = map(int, re.findall(r"\d+", lines[0]))
+    distances = map(int, re.findall(r"\d+", lines[1]))
 
+    for time, distance in zip(times, distances):
+        runs = 0
+
+        for millisecond in range(0, time + 1):
+            actual = (time - millisecond) * millisecond
+            if actual > distance:
+                runs += 1
+
+        value *= runs
     return value
 
 
 def part02(lines):
     value = 0
+    time = int("".join(re.findall(r"\d+", lines[0])))
+    distance = int("".join(re.findall(r"\d+", lines[1])))
+
+    for millisecond in range(0, time + 1):
+        actual = (time - millisecond) * millisecond
+        if actual > distance:
+            value += 1
 
     return value
 
