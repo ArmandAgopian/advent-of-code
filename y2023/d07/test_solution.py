4241fb731fc9124771da68dbbed95d12bb0ab60e
Armand Agopian <armandagopian457@gmail.com>
2024-05-06 15:58:48 -0700

    day 7 prep

---
+++ y2023/d07/test_solution.py
@@ -0,0 +1,13 @@
+from .solution import *
+
+
+def test_part01():
+    example = ["32T3K 765", "T55J5 684", "KK677 28", "KTJJT 220", "QQQJA 483"]
+
+    assert part01(example) == 6440
+
+
+def test_part02():
+    example = ["32T3K 765", "T55J5 684", "KK677 28", "KTJJT 220", "QQQJA 483"]
+
+    # assert part02(example) ==
