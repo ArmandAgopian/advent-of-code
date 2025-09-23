757e9420f39103e9f07a225233dc51be54dc2209
Armand Agopian <armandagopian457@gmail.com>
2025-09-22 23:37:05 -0700

    Day 8 done

---
+++ 2023/08/day08.py
@@ -0,0 +1,47 @@
+import itertools
+from math import lcm
+import regex as re
+from pathlib import Path
+
+
+def part1(lines):
+    tree = {}
+    for line in lines[2:]:
+        key = line[:3]
+        val1 = line[7:10]
+        val2 = line[12:15]
+        tree[key] = (val1, val2)
+
+    curr = 'AAA'
+    for i, n in zip(itertools.count(), itertools.cycle(lines[0])):
+        if curr == 'ZZZ':
+            return i
+        curr = tree[curr][0 if n == 'L' else 1]
+
+def part2(lines):
+    tree = {}
+    for line in lines[2:]:
+        key = line[:3]
+        val1 = line[7:10]
+        val2 = line[12:15]
+        tree[key] = (val1, val2)
+
+    pattern = re.compile(r"..A")  # 3 chars, last must be A
+    curr = [k for k in tree.keys() if pattern.match(k)]
+    completed = []
+
+    for node in curr:
+        for i, n in zip(itertools.count(), itertools.cycle(lines[0])):
+            if node[2] == 'Z':
+                completed.append(i)
+                break
+            node = tree[node][0 if n == 'L' else 1]
+
+    return lcm(*completed)
+
+if __name__ == "__main__":
+    with open(Path(__file__).parent / "input.txt") as f:
+        lines = f.read().splitlines()
+
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
---
+++ 2023/08/test_day08.py
@@ -0,0 +1,35 @@
+import pytest
+from day08 import part1, part2
+
+INPUT_PART1 = """\
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+"""
+
+EXPECTED_PART1 = 6
+
+INPUT_PART2="""\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
+
+EXPECTED_PART2 = 6
+
+def test_part1():
+    lines = INPUT_PART1.splitlines()
+    assert part1(lines) == EXPECTED_PART1
+
+def test_part2():
+    lines = INPUT_PART2.splitlines()
+    assert part2(lines) == EXPECTED_PART2
