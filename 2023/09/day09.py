3ccb34388f7b8ff6dba1611291036d1a19b9ab1b
Armand Agopian <armandagopian457@gmail.com>
2025-09-23 20:36:57 -0700

    Day 09 complete

---
+++ 2023/09/day09.py
@@ -0,0 +1,41 @@
+import itertools
+from math import lcm
+import regex as re
+from pathlib import Path
+
+def find_history_1(line):
+    if len(set(line)) == 1:
+        return line[-1]
+    
+    diffs = [line[i+1] - line[i] for i in range(len(line)-1)]
+    next_diff = find_history_1(diffs)
+
+    return line[-1] + next_diff
+
+def find_history_2(line):
+    if len(set(line)) == 1:
+        return line[0]
+    
+    diffs = [line[i] - line[i-1] for i in range(1, len(line))]
+    next_diff = find_history_2(diffs)
+
+    return line[0] - next_diff
+
+def part1(lines):
+    histories = []
+    for line in lines:
+        histories.append(find_history_1(list(map(int,line.split()))))
+    return sum(histories)
+
+def part2(lines):
+    histories = []
+    for line in lines:
+        histories.append(find_history_2(list(map(int,line.split()))))
+    return sum(histories)
+
+if __name__ == "__main__":
+    with open(Path(__file__).parent / "input.txt") as f:
+        lines = f.read().splitlines()
+
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
---
+++ 2023/09/test_day09.py
@@ -0,0 +1,26 @@
+import pytest
+from day09 import part1, part2
+
+INPUT_PART1 = """\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+"""
+
+EXPECTED_PART1 = 114
+
+INPUT_PART2="""\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+"""
+
+EXPECTED_PART2 = 2
+
+def test_part1():
+    lines = INPUT_PART1.splitlines()
+    assert part1(lines) == EXPECTED_PART1
+
+def test_part2():
+    lines = INPUT_PART2.splitlines()
+    assert part2(lines) == EXPECTED_PART2
