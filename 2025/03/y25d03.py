38ccbfeffcc3d1562e9c98de79a6e4b0b4635303
Armand Agopian <armandagopian457@gmail.com>
2025-12-20 02:28:49 -0800

    feat(solution):
    Signed-off-by: Armand Agopian <armandagopian457@gmail.com>

---
+++ 2025/03/y25d03.py
@@ -0,0 +1,45 @@
+import heapq
+from pathlib import Path
+import regex as re
+
+
+def part1(input: str) -> int:
+    value = 0
+
+    for line in input.splitlines():
+        bank = [int(i) for i in line]
+        highest = max(bank[:-1])
+        i = bank.index(highest)
+        second_highest = max(bank[i+1:])
+        value += (highest * 10) + second_highest
+
+    return value
+
+def part2(input: str) -> int:
+    # i is 0
+    # list of values
+    #for reverse range of 12->1?
+    # find max, repeat, update i. subset of [i+1:range limit]
+    value = 0
+
+    for line in input.splitlines():
+        bank = [int(i) for i in line]
+        digits = []
+        i = -1
+
+        for num in range(-11, 1):
+            if num == 0:
+                num = len(bank)
+
+            highest = max(bank[i + 1:num])
+            digits.append(highest)
+            i = bank.index(highest, i + 1, num)
+        value += int("".join(map(str, digits)))
+
+    return value
+
+if __name__ == "__main__":
+    input = (Path(__file__).parent / "input.txt").read_text()
+
+    print(f"Part 1: {part1(input)}")
+    print(f"Part 2: {part2(input)}")
