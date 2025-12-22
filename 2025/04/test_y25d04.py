f1c91d8a4f2a885dd2ad2eaa2dd3c1858dda9573
Armand Agopian <armandagopian457@gmail.com>
2025-12-22 15:14:20 -0800

    feat(solution): Day 4 complete
    Signed-off-by: Armand Agopian <armandagopian457@gmail.com>

---
+++ 2025/04/test_y25d04.py
@@ -0,0 +1,38 @@
+import pytest
+from y25d04 import part1, part2
+
+INPUT_PART1 = """\
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+
+EXPECTED_PART1 = 13
+
+INPUT_PART2= """\
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+
+EXPECTED_PART2 = 43
+
+def test_part1():
+    assert part1(INPUT_PART1) == EXPECTED_PART1
+
+def test_part2():
+    assert part2(INPUT_PART2) == EXPECTED_PART2
---
+++ 2025/04/y25d04.py
@@ -0,0 +1,67 @@
+import heapq
+from pathlib import Path
+import regex as re
+
+
+def part1(input: str) -> int:
+    grid = [list(line) for line in input.splitlines()]
+    value = 0
+
+    directions = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),           (0, 1),
+                  (1, -1),  (1, 0),  (1, 1)]
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == '@':
+                neighbors = 0
+
+                for drow, dcol in directions:
+                    row_new = row + drow
+                    col_new = col + dcol
+                    if (0 <= row_new < len(grid)) and (0 <= col_new < len(grid[0])) and (grid[row_new][col_new] == '@'):
+                        neighbors += 1
+
+                if neighbors < 4:
+                    value += 1
+    return value
+
+def part2(input: str) -> int:
+    grid = [list(line) for line in input.splitlines()]
+    value = 0
+    iteration = 1
+
+    directions = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),           (0, 1),
+                  (1, -1),  (1, 0),  (1, 1)]
+
+    while iteration > 0:
+        iteration = 0
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == '@':
+                    neighbors = 0
+
+                    for drow, dcol in directions:
+                        row_new = row + drow
+                        col_new = col + dcol
+                        if (0 <= row_new < len(grid)) and (0 <= col_new < len(grid[0])) and (grid[row_new][col_new] == '@' or grid[row_new][col_new] == 'x'):
+                            neighbors += 1
+
+                    if neighbors < 4:
+                        value += 1
+                        grid[row][col] = 'x'
+                        iteration += 1
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 'x':
+                    grid[row][col] = '.'
+    return value
+
+if __name__ == "__main__":
+    input = (Path(__file__).parent / "input.txt").read_text()
+
+    print(f"Part 1: {part1(input)}")
+    print(f"Part 2: {part2(input)}")
